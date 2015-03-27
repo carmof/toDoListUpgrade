d25be786b86fb192d792c25cb3984c115c102db7
carmof <carmof@hanover.edu>
2015-03-27 15:12:49 -0400

    Applying canvas

--- app/game.js
+++ app/game.js
@@ -1,36 +1,54 @@
 
-var Game, proto;
+define(["jquery"],
+function($) {
 
-/*
-*       Constructors
-*/
+	var Game, proto, canvasWidth, canvasHeight;
 
-function makeNewGame() {
-	var hist = Object.create(proto);
-	hist.list = DLList.new();
-	hist.current = null;
-	return hist;
-}
+	/*
+	*       Constructors
+	*/
+
+	function makeNewGame(canvasId, canvasHgt, canvasWth) {
+		var base = Object.create(proto, {
+			canvas: {
+		        enumerable: true,
+		        configurable: true,
+		        writeable: true,
+		        value: $("#" + canvasId)[0]
+		    },
+			context: {
+		        enumerable: true,
+		        configurable: true,
+		        writeable: true,
+		        value: $("#" + canvasId)[0].getContext("2d")
+		    }
+		});
+		return base;
+	}
 
 
-/*
-*       Prototype / Instance methods
-*/
+	/*
+	*       Prototype / Instance methods
+	*/
 
-proto = {
-   
-};
+	proto = {
+		clearCanvas: function(){
+			this.context.clearRect(0,0,canvasWidth,canvasHeight);
+		}
+	};
 
 
 
-// DO NOT MODIFY ANYTHING BELOW THIS LINE
-Game = {
-new: makeNewGame
-};
+	// DO NOT MODIFY ANYTHING BELOW THIS LINE
+	Game = {
+		new: makeNewGame
+	};
 
-Object.defineProperty(Game, "prototype", {
-value: proto,
-writable: false
+	Object.defineProperty(Game, "prototype", {
+	value: proto,
+	writable: false
+	});
+
+   return Game;
+
 });
-
-module.exports = Game;
--- app/main.js
+++ app/main.js
@@ -9,12 +9,7 @@ require.config({
    paths: {
       "lib": "../lib",
       "jquery": "../lib/jquery",
-      "handlebars": "../lib/handlebars-v3.0.0",
-      "ball": "/ball",
-      "bricks": "bricks",
-      "game": "game",
-      "directions": "directions",
-      "pad": "pad",
+      "handlebars": "../lib/handlebars-v3.0.0"
       // or the following to load remotely:
       // "jquery": "https://code.jquery.com/jquery-2.1.3.min"
    },
@@ -22,13 +17,13 @@ require.config({
 });
 
 // All other modules should be called through here
-require(["jquery", "ball","bricks","game","directions","pad"],
-function($, Ball, Brick, Game, Directions, Pad) {
+require(["jquery", "handlebars", "app/game.js"],
+function($, handleBars, Game) {
    // mixin
    $(function() {
       console.log("Page loaded!");
-      console.log(Ball);
+      var a = Game.new("backgroundCanvas", 500, 500);
    });
-   console.log("Other things here: Ball");
+   console.log("Other things here");
    
 });
