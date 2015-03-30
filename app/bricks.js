c41fb23b738727ae4e41e7ecda9562904ce45da5
carmof <carmof@hanover.edu>
2015-03-30 10:51:43 -0400

    working on bricks

--- app/bricks.js
+++ app/bricks.js
@@ -4,10 +4,20 @@ var Brick, proto;
 *       Constructors
 */
 
-function makeNewBrick() {
-   var game = Object.create(proto);
-   return game;
-}
+function makeNewBrick(height, width, color) {
+   var brick = Object.create(proto,{
+      object: {
+         enumerable: true,
+         configurable: true,
+         writeable: true,
+         value: document.createElementNS("http://www.w3.org/2000/svg", "rect");
+      }
+   }
+   brick.object.setAttribute("width", height);
+   brick.object.setAttribute("height", width);
+   brick.object.setAttribute("fill", color);
+   return brick;
+   );
 
 
 /*
@@ -15,7 +25,9 @@ function makeNewBrick() {
 */
 
 proto = {
-   
+   drawOnScreen: function(x, y){
+
+   }
 };
 
 
